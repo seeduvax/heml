1c91407d287f2ca544d10cf38a376e65ab4bdd11
Sebastien Devaux <sebastien.devaux@eduvax.net>
2020-11-14 14:48:48 +0100

    issue management

---
+++ _charm/src/7e3377158c95a05673a0600bd22f0bf37fea8a00342ef25cae7ee5b0b7a3e182.cr
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="utf-8"?>
+<cr id="7e3377158c95a05673a0600bd22f0bf37fea8a00342ef25cae7ee5b0b7a3e182" state="open">
+<title>Release heml 1.0</title>
+<reporter>sdevaux</reporter>
+<creation>2020-11-14 14:48:47+01:00</creation>
+<description></description>
+<links>
+<link name="parent"></link>
+<link name="child">af494d16632fc70a29aeeeff7ae188acbee5c8bb4ac300721629f1d907054c7b</link>
+</links>
+<cf v="null"/>
+</cr>
---
+++ _charm/src/af494d16632fc70a29aeeeff7ae188acbee5c8bb4ac300721629f1d907054c7b.cr
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<cr id="af494d16632fc70a29aeeeff7ae188acbee5c8bb4ac300721629f1d907054c7b" state="open">
+<title>in-processing scripting</title>
+<reporter>sdevaux</reporter>
+<creation>2020-11-14 14:48:47+01:00</creation>
+<description></description>
+<links>
+<link name="parent">7e3377158c95a05673a0600bd22f0bf37fea8a00342ef25cae7ee5b0b7a3e182</link>
+</links>
+<cf v="null"/>
+</cr>
