eac71daa561837a6b8f9b4b7a616fc51b69a34da
Sebastien Devaux <sebastien.devaux@eduvax.net>
2020-11-18 15:37:30 +0100

    #7e33771 issue management

--- _charm/src/7e3377158c95a05673a0600bd22f0bf37fea8a00342ef25cae7ee5b0b7a3e182.cr
+++ _charm/src/7e3377158c95a05673a0600bd22f0bf37fea8a00342ef25cae7ee5b0b7a3e182.cr
@@ -12,6 +12,7 @@
 <link name="child">a40e94fc51d8c4009c26e952a16339d45a805fff</link>
 <link name="child">af494d16632fc70a29aeeeff7ae188acbee5c8bb4ac300721629f1d907054c7b</link>
 <link name="child">bb5cfd15a0d12767ccaf18014654337e604169845d49ba93ea922664682cb0b9</link>
+<link name="child">98909f676a31bd233bddb91b988eb133acc94186be6ffededa4733a8f8122be9</link>
 </links>
 <cf v="sdevaux 2020-11-16T22:30:31+01:00"/>
 </cr>
---
+++ _charm/src/98909f676a31bd233bddb91b988eb133acc94186be6ffededa4733a8f8122be9.cr
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<cr id="98909f676a31bd233bddb91b988eb133acc94186be6ffededa4733a8f8122be9" state="open">
+<title>add capability to include fragments of external files</title>
+<reporter>sdevaux</reporter>
+<creation>2020-11-18 15:37:30+01:00</creation>
+<description></description>
+<links>
+<link name="parent">7e3377158c95a05673a0600bd22f0bf37fea8a00342ef25cae7ee5b0b7a3e182</link>
+</links>
+<cf v="null"/>
+</cr>
