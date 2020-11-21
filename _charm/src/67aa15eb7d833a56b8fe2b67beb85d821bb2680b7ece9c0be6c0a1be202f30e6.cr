f167c8a0e96f6d37f4038ae4d4bbab3e2c71bac5
Sebastien Devaux <sebastien.devaux@eduvax.net>
2020-11-21 17:29:44 +0100

    #67aa15e adding style and logo

--- _charm/src/67aa15eb7d833a56b8fe2b67beb85d821bb2680b7ece9c0be6c0a1be202f30e6.cr
+++ _charm/src/67aa15eb7d833a56b8fe2b67beb85d821bb2680b7ece9c0be6c0a1be202f30e6.cr
@@ -7,5 +7,5 @@
 <links>
 <link name="parent">7e3377158c95a05673a0600bd22f0bf37fea8a00342ef25cae7ee5b0b7a3e182</link>
 </links>
-<cf v="sdevaux 2020-11-21T17:29:04+01:00"/>
+<cf v="sdevaux 2020-11-21T17:29:59+01:00"/>
 </cr>
