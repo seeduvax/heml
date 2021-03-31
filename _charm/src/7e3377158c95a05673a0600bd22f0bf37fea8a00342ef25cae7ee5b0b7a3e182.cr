f706af93a969ddb3e9b8f5ff590ae1f333c622d4
Sebastien Devaux <sebastien.devaux@eduvax.net>
2021-03-31 18:35:05 +0200

    #7e33771  #7e33771  [tag switch] publish dep management improvements.

--- _charm/src/7e3377158c95a05673a0600bd22f0bf37fea8a00342ef25cae7ee5b0b7a3e182.cr
+++ _charm/src/7e3377158c95a05673a0600bd22f0bf37fea8a00342ef25cae7ee5b0b7a3e182.cr
@@ -17,5 +17,5 @@
 <link name="child">41c7d3cff3d38d6b5e0e16a5c7cc01bc6216084d8dbe31d01e65f84c6a9ec406</link>
 <link name="child">bd1a49df5fffbfd0b9dde28f6d573455d96b1293facc36157d0fa41653987282</link>
 </links>
-<cf v="sdevaux 2020-11-28T16:46:14+01:00"/>
+<cf v="sdevaux 2021-03-31T18:35:05+02:00"/>
 </cr>
