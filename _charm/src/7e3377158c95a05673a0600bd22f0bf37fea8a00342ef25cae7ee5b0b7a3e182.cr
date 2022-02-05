1b1a7344cf9ca1043c9f0601ad1301b143b12224
Sebastien Devaux <sebastien.devaux@eduvax.net>
2022-02-05 14:37:55 +0100

    #7e33771 issue management

--- _charm/src/7e3377158c95a05673a0600bd22f0bf37fea8a00342ef25cae7ee5b0b7a3e182.cr
+++ _charm/src/7e3377158c95a05673a0600bd22f0bf37fea8a00342ef25cae7ee5b0b7a3e182.cr
@@ -19,6 +19,7 @@
 <link name="child">efe8fe2b1f99acef9cb0543dab3cf11f7a856c73107786b02367320c195c37d0</link>
 <link name="child">81c2b449fbadf54af8bad152c3f1cc93ac7d0ef565d66c19c0b808845ef0f2ce</link>
 <link name="child">5d9080ef981344c9aa7cc1a35ae0307d4f1fdacb8e6d35124e8b2db3ebb456bf</link>
+<link name="child">885e4e8a8f27bd973781f6fa171af7ec44fcf5e1e8426c39d5822bd132634b4e</link>
 </links>
 <cf v="sdevaux 2022-01-02T12:42:56+01:00"/>
 </cr>
---
+++ _charm/src/885e4e8a8f27bd973781f6fa171af7ec44fcf5e1e8426c39d5822bd132634b4e.cr
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<cr id="885e4e8a8f27bd973781f6fa171af7ec44fcf5e1e8426c39d5822bd132634b4e" state="open">
+<title>dictionary key substitution is not working in attributes</title>
+<reporter>sdevaux</reporter>
+<creation>2022-02-05 14:37:55+01:00</creation>
+<description></description>
+<links>
+<link name="parent">7e3377158c95a05673a0600bd22f0bf37fea8a00342ef25cae7ee5b0b7a3e182</link>
+</links>
+<cf v="null"/>
+</cr>
