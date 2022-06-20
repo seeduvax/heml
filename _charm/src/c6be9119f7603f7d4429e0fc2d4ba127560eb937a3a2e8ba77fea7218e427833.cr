80570dbf449268b056cb8e7505ea97cbe1a575d8
Sebastien Devaux <sebastien.devaux@ariane.group>
2022-06-20 18:20:07 +0200

    #7e33771 issue management

---
+++ _charm/src/c6be9119f7603f7d4429e0fc2d4ba127560eb937a3a2e8ba77fea7218e427833.cr
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<cr id="c6be9119f7603f7d4429e0fc2d4ba127560eb937a3a2e8ba77fea7218e427833" state="open">
+<title>Element name trailing space unexpectedly makes inline element</title>
+<reporter>m026258</reporter>
+<creation>2022-06-20 18:20:07+02:00</creation>
+<description></description>
+<links>
+<link name="parent">7e3377158c95a05673a0600bd22f0bf37fea8a00342ef25cae7ee5b0b7a3e182</link>
+</links>
+<cf v="null"/>
+</cr>
