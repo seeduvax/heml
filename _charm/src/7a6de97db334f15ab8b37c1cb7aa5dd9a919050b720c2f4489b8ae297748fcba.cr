d4e1646973049e3ade4333f698ac19d9bf5ab396
Sebastien Devaux <sebastien.devaux@eduvax.net>
2023-04-10 18:58:14 +0200

    issue management

---
+++ _charm/src/7a6de97db334f15ab8b37c1cb7aa5dd9a919050b720c2f4489b8ae297748fcba.cr
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="utf-8"?>
+<cr id="7a6de97db334f15ab8b37c1cb7aa5dd9a919050b720c2f4489b8ae297748fcba" state="open">
+<title>Release heml 2.0</title>
+<reporter>sdevaux</reporter>
+<creation>2023-04-10 18:58:14+02:00</creation>
+<description></description>
+<links>
+<link name="parent"></link>
+<link name="child">a4299d76e69ee6969c00980c6dd9aa48ed3c246dd3e7b95267d927ba718be182</link>
+</links>
+<cf v="null"/>
+</cr>
---
+++ _charm/src/a4299d76e69ee6969c00980c6dd9aa48ed3c246dd3e7b95267d927ba718be182.cr
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<cr id="a4299d76e69ee6969c00980c6dd9aa48ed3c246dd3e7b95267d927ba718be182" state="open">
+<title>basic file reading loop in rust</title>
+<reporter>sdevaux</reporter>
+<creation>2023-04-10 18:58:14+02:00</creation>
+<description></description>
+<links>
+<link name="parent">7a6de97db334f15ab8b37c1cb7aa5dd9a919050b720c2f4489b8ae297748fcba</link>
+</links>
+<cf v="null"/>
+</cr>
