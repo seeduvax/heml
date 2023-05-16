a619184d8f7e96c5d7d3c905ae8a5545c7680aad
Sebastien Devaux <sebastien.devaux@eduvax.net>
2023-05-16 14:00:24 +0200

    #7a6de97 issue management

---
+++ _charm/src/7f983bb50914afba26f5d52d6ab5aea40f9f70e418e974a52c291042e2122333.cr
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<cr id="7f983bb50914afba26f5d52d6ab5aea40f9f70e418e974a52c291042e2122333" state="open">
+<title>XML serialization handler</title>
+<reporter>sdevaux</reporter>
+<creation>2023-05-16 14:00:24+02:00</creation>
+<description></description>
+<links>
+<link name="parent">7a6de97db334f15ab8b37c1cb7aa5dd9a919050b720c2f4489b8ae297748fcba</link>
+</links>
+<cf v="null"/>
+</cr>
